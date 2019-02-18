9f8ed250981a497f70a7bfab305a4b155a40f221
Edgar Meruvia <emeruvia@gmail.com>
2019-02-18 18:23:38 -0500

    Fix issue with duplicate code

--- intermediate/Intermediate/Program.cs
+++ intermediate/Intermediate/Program.cs
@@ -68,7 +68,7 @@ namespace Intermediate
                 }
                 else
                 {
-                    newPost.Downvote();
+                    newPost.Upvote();
                 }
 
                 i--;
