56014e7d66f5c6d2a32a52336fcec74ccee7c4f0
Edgar Meruvia <emg.appdev@gmail.com>
2020-01-08 00:32:48 -0500

    Sample API project init

---
+++ SampleAPI/SampleAPI/Models/TodoContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SampleAPI.Models
+{
+    public class TodoContext
+    {
+        public TodoContext()
+        {
+        }
+    }
+}
---
+++ SampleAPI/SampleAPI/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace SampleAPI.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+
+    }
+}
